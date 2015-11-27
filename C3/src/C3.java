225f65cd232f37f4ac42ee2628801c15d45f35b0
Cody Smith <smartkid101@gmail.com>
2015-11-26 18:39:30 -0700

    Initial commit

---
+++ C3/src/C3.java
@@ -0,0 +1,133 @@
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+ 
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+ 
+public class C3 {
+ 
+    // We need to strongly reference callback instances.
+    private GLFWErrorCallback errorCallback;
+    private GLFWKeyCallback   keyCallback;
+ 
+    // The window handle
+    private long window;
+ 
+    public void run() {
+    	
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+ 
+        try {
+        	
+            init();
+            
+            loop();
+ 
+            // Release window and window callbacks
+            glfwDestroyWindow(window);
+            
+            keyCallback.release();
+            
+        } finally {
+        	
+            // Terminate GLFW and release the GLFWErrorCallback
+            glfwTerminate();
+            
+            errorCallback.release();
+            
+        }
+        
+    }
+ 
+    private void init() {
+    	
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
+ 
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (glfwInit() != GLFW_TRUE)
+            throw new IllegalStateException("Unable to initialize GLFW");
+ 
+        // Configure our window
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+ 
+        int WIDTH = 1024;
+        
+        int HEIGHT = 768;
+ 
+        // Create the window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+        
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+ 
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+        	
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+            	
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                    glfwSetWindowShouldClose(window, GLFW_TRUE); // We will detect this in our rendering loop
+                
+            }
+            
+        });
+ 
+        // Get the resolution of the primary monitor
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        
+        // Center our window
+        glfwSetWindowPos(window, (vidmode.width() - WIDTH) / 2, (vidmode.height() - HEIGHT) / 2);
+ 
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        
+        // Enable v-sync
+        glfwSwapInterval(1);
+ 
+        // Make the window visible
+        glfwShowWindow(window);
+        
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+        
+    }
+ 
+    private void loop() {
+    	
+        // Set the clear color
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+ 
+        // Render loop
+        while (glfwWindowShouldClose(window) == GLFW_FALSE) {
+        	
+        	// clear the framebuffer
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ 
+            // swap the color buffers
+            glfwSwapBuffers(window); 
+ 
+            // Poll for window / key events.
+            glfwPollEvents();
+            
+        }
+        
+    }
+ 
+    public static void main(String[] args) {
+    	
+        new C3().run();
+        
+    }
+ 
+}
